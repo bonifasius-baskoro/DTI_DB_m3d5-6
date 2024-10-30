f68a182a2e27c11540c7305340b1d755f3033f8b
Jerry Baskoro <bjbbeat@gmail.com>
2024-10-30 16:50:06 +0700

    add order_price_component for order price component that will be summed for total price

--- dummy_insert.sql
+++ dummy_insert.sql
@@ -86,11 +86,61 @@ INSERT INTO invoice_test.delivery_data (vendor_id, status, order_id) VALUES
 (3, 'Processing', 3),
 (4, 'Picked Up', 4);
 
+TRUNCATE TABLE order_price RESTART IDENTITY;
+commit;
+select * from order_price op ;
+
+insert into order_price (order_id, fee_type_id, fee_price)
+select opc.order_id order_id , opc.fee_type_id fee_type_id,
+case when ft.fee_rate_type  = 1 then ft.rate * p.price 
+when fee_rate_type =2 then ft.rate end as fee_price 
+from order_invoice oi 
+join order_price_component opc on opc.order_id = oi.id 
+join product p  on oi.product_id = p.id 
+join fee_type ft on opc.fee_type_id = ft.id ;
+
+
+select * from fee_type ft ;
+select * from order_price op ;
+
+select * from order_invoice oi  join order_price_component opc on opc.order_id = oi.id
+join product p on oi.product_id  = ;
+
+
+create table order_price_component as 
+select order_id ,fee_type_id from order_price;
+
+insert  into order_price_component (order_id, fee_type_id) values 
+(1,5),(2,5),(3,5),(4,5);
+
+alter table   order_price_component ADD CONSTRAINT feecomp_order_fk  foreign key (order_id)  references order_invoice(id);
+truncate table order_price ;
+
+select * from order_invoice oi ;
+
+with product as (
+select p.id , p.price
+
+from product p 
+)
+
+create table order_fee_part as 
+with product as (p.id , p.price from product p),
+fee_detail as (select from fee_type ft)
+
+-- 12. Insert delivery_data data
+INSERT INTO invoice_test.delivery_data (vendor_id, status, order_id) VALUES
+(1, 'In Transit', 1),
+(2, 'Delivered', 2),
+(3, 'Processing', 3),
+(4, 'Picked Up', 4);
+
 
 select oi.id , user1."name"  as name_user,
 p.amount  payment_amount,
 pt."name"  payment_type,
 dv."name" as delivery_vendor_name ,
+ua.street_name as street_name,
 dd.status as delivery_status from order_invoice oi 
 join user_invoice user1 on oi.user_id  = user1.id 
 join payment p on oi.payment_id  = p.id 
@@ -98,7 +148,38 @@ join payment_type pt  on p.payment_type_id  = pt.id
 join status_payment sp  on p.status_id  = sp.id 
 join product pr on pr.id  = oi.product_id 
 join seller s on pr.seller_id  = s.id
+join user_address ua  on ua.user_id  = user1.id
 join delivery_data dd  on dd.id  = oi.id  
 join delivery_vendor dv  on dd.vendor_id  =dv.id ;
 
+
 select * from delivery_data dd;
+
+SELECT 
+    oi.id,
+    user1.name AS name_user,
+    SUM(op.fee_price + pr.price) AS total_price,
+    p.amount AS payment_amount,
+    pt.name AS payment_type,
+    dv.name AS delivery_vendor_name,
+    ua.street_name AS street_name,
+    dd.status AS delivery_status 
+FROM order_invoice oi
+JOIN user_invoice user1 ON oi.user_id = user1.id
+JOIN order_price op ON oi.id = op.order_id
+JOIN payment p ON oi.payment_id = p.id
+JOIN payment_type pt ON p.payment_type_id = pt.id
+JOIN status_payment sp ON p.status_id = sp.id
+JOIN product pr ON pr.id = oi.product_id
+JOIN seller s ON pr.seller_id = s.id
+JOIN user_address ua ON ua.user_id = user1.id
+JOIN delivery_data dd ON dd.order_id = oi.id  -- Changed this line
+JOIN delivery_vendor dv ON dd.vendor_id = dv.id
+GROUP BY 
+    oi.id,
+    user1.name,
+    p.amount,
+    pt.name,
+    dv.name,
+    ua.street_name,
+    dd.status;
